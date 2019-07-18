f53e8109d550aedc7683cace1ef0c200158895d2
Rulo Kobashikawa <akobashikawa@gmail.com>
2019-07-18 17:13:55 -0500

    Implementa simple rt message list

--- src/main.js
+++ src/main.js
@@ -2,6 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyClPhm0yIaC5PkIpcF8o7g5HVaATqKWqls",
+  authDomain: "rulokoba-vue-firebase-chat.firebaseapp.com",
+  databaseURL: "https://rulokoba-vue-firebase-chat.firebaseio.com",
+  projectId: "rulokoba-vue-firebase-chat",
+  storageBucket: "",
+  messagingSenderId: "250359732445",
+  appId: "1:250359732445:web:fa24bb533a75cca8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+window.db = db;
 
 Vue.config.productionTip = false
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Simple Chat con mi Aviva Coach</h1>
   </div>
 </template>
